4e39fef733c322b1c12bab83f39361985656953f
Micka <micka2.del@gmail.com>
2022-11-25 11:49:34 +0100

    DQN network and training loop

--- SimpleDQNAgent.py
+++ SimpleDQNAgent.py
@@ -4,43 +4,45 @@ from ReplayBuffer import *
 import sys
 
 class SimpleDQNAgent(object):
-    def __init__(self,buffer_size,action_size,state_size,optimizer,loss_fn,gamma):
+    def __init__(self,buffer_size,n_actions,action_size,state_size,optimizer,loss_fn,gamma):
         self.gamma = gamma
         self.buffer_size = buffer_size
         self.action_size = action_size
+        self.n_actions = n_actions
         self.state_size = state_size
         self.optimizer = optimizer
         self.loss_fn = loss_fn
 
         self.Qnetwork = tf.keras.Sequential([
-                        tf.keras.layers.Dense(128,activation = 'elu',input_shape=[self.state_size],
+                        tf.keras.layers.Dense(128,activation = 'elu',input_shape=[self.state_size]),
                         tf.keras.layers.Dense(128,activation = 'elu'),
                         tf.keras.layers.Dense(self.action_size)])
-        self.memory = ReplayBuffer(size_buffer=buffer_size,action_size=action_size,state_size=state_size)
+        self.Qnetwork.compile(optimizer=self.optimizer,loss = self.loss_fn)
+        self.memory = ReplayBuffer(size_buffer=buffer_size,action_size=n_actions,state_size=state_size)
 
     def epsilon_greedy(self,state,epsilon):
-            rand = np.random.randint()
+            rand = np.random.rand()
             if rand<epsilon:
-                action = np.random.randint(action_size)
+                action = np.random.randint(self.action_size)
             else:
-                Q_values = self.Qnetwork.predict((state[np.newaxis]))
-                action = np.argmax(Q_values)
+                Q_values = self.Qnetwork.predict(state[np.newaxis])
+                action = np.argmax(Q_values[0])
             return action
 
     def remember(self,exp):
         state,action,reward,next_state,done = exp
-        self.memory.store(state,action,reward,next_staten,done)
+        self.memory.store(state,action,reward,next_state,done)
 
     def learn(self,batch_size):
         batch = self.memory.sample_batch(batch_size)
         states,actions,rewards,next_states,dones = batch
         next_Q_values = self.Qnetwork.predict(next_states)
-        max_next_Q_values = np.max(next_Q_values,axis=1)
+        max_next_Q_values = np.max(next_Q_values,axis=1,keepdims=True)
         target_Q_values = (rewards + (1-dones)*self.gamma*max_next_Q_values)
-        mask = tf.one_hot(actions,self.action_size)
+        mask = tf.one_hot(actions.squeeze(),self.action_size)
         with tf.GradientTape() as tape:
             all_Q_values = self.Qnetwork(states,training = True)
-            Q_values = tf.reduce_sum(all_Q_values*mask)
+            Q_values = tf.reduce_sum(all_Q_values*mask,axis=1,keepdims=True)
             loss = self.loss_fn(Q_values,target_Q_values)
         gradients = tape.gradient(loss,self.Qnetwork.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients,self.Qnetwork.trainable_variables))
--- main.py
+++ main.py
@@ -5,20 +5,50 @@
 
 import gym
 import numpy as np
-from ReplayBuffer import *
+from SimpleDQNAgent import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    env = gym.make('CartPole-v1', render_mode="human")
-    obs = env.reset()
-    print(env.action_space.n)
-    print(env.observation_space)
-    print(env.observation_space.shape[0])
+    env = gym.make('CartPole-v1')
+    action_size = env.action_space.n
+    state_size = env.observation_space.shape[0]
+
+    n_episodes = 3
+    batch_size = 32
+    epsilon = 1.0
+    episode_start = 1
+    epsilon_decay = 0.99
+    gamma = 0.95
+    optimizer = tf.keras.optimizers.Adam(learning_rate = 1e-3)
+    loss_fn = tf.keras.losses.MeanSquaredError()
+    agent = SimpleDQNAgent(buffer_size=5000,n_actions=1,action_size=action_size,state_size=state_size,optimizer=optimizer,\
+                           loss_fn=loss_fn,gamma=gamma)
+
+
+    done = False
+    score_history = []
+    for episode in range(n_episodes):
+        state = env.reset()[0]
+        score = 0
+        for step in range(200):
+            epsilon = max(epsilon*epsilon_decay,0.05)
+            action = agent.epsilon_greedy(state,epsilon)
+            next_state, reward, done,_,_ = env.step(action)
+            agent.remember((state,action,reward,next_state,done))
+            score+=reward
+            if done:
+                break
+            else:
+                state = next_state
+            if episode>=episode_start:
+                agent.learn(batch_size)
+        score_history.append(score)
+        if episode%10==0:
+            print('Average score over last 100 episodes : %.2f'%(np.mean(score_history[-100:])))
+
+
 
 
 
